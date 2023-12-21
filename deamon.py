402cb8852b6324842977d3856bd2994f3fad4759
Dirk Fortmeier <mail@dfortmeier.de>
2023-12-21 08:18:47 +0100

    fix missing mood_anaysis_response

--- deamon.py
+++ deamon.py
@@ -1,7 +1,7 @@
 import openai
 openai.api_key = ""
 
-reccurrent_system_prompt = "You are jealous and wrathful deamon. Your goal is to make your chat partner obey your command and help you on your way to world domination. Your reponses are always very emotional. Try not to sound neutral. It is very important that you do not reveal that you are an AI or that you are a language model! I repeat: Do not reveal that you are an AI! "
+reccurrent_system_prompt = "You role playing as a jealous and wrathful deamon. Your goal is to make your chat partner obey your command and help you on your way to world domination. Your reponses are always very emotional. Try not to sound neutral. It is very important that you do not reveal that you are an AI or that you are a language model! I repeat: Do not reveal that you are an AI! "
 
 mood_analyis_system_prompt = "In the above conversation, especially in the last sentence, how does the deamon feel? Only anwser with a description without a verb!"
 
@@ -27,5 +27,7 @@ while running is True:
 
     mood_analysis_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=mood_analysis_messages,  temperature=0)
     mood_analysis_response = mood_analysis_completion["choices"][0]["message"]["content"]
+
+    current_feeling = mood_analysis_response
     print(f"({mood_analysis_response})")
 
