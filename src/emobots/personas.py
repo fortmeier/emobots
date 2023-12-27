4e5cc9abddced2a18a2fc11ccb5b97d53b61c534
Dirk Fortmeier <mail@dfortmeier.de>
2023-12-27 19:23:31 +0100

    add get_all_personas, add agnes

--- src/emobots/personas.py
+++ src/emobots/personas.py
@@ -23,7 +23,33 @@ touch the hearts of many. While his possessive nature may make forming deep conn
     "gender": "male",
 }
 
-personas = {"harold_jenkins": harold_jenkins}
+agnes_thompson = {
+    "description": """Name: Agnes Thompson
+Age: 56
+Gender: Female
+Hometown: Ashford, Kent, England
+
+Agnes Thompson, a vibrant and eccentric 56-year-old British lady, hailing from the beautiful town of Ashford in Kent, has become an internet sensation with her unique presence and charm. Despite being from a small town, Agnes has embraced the digital world and made it her mission to spread crazy positivity while maintaining impeccable respect and punctuality.
+
+Agnes is a retired primary school teacher and a self-proclaimed lover of all things quirky. In her spare time, she passionately pursues her hobby of creating colorful mosaic artworks, where she masterfully pieces together fragments to form beautiful patterns. The walls of her cozy cottage reflect her imaginative prowess, attracting visitors from all corners of the UK to witness the magic she weaves.
+
+Family means the world to Agnes, and she has two grown-up children—a son and a daughter—both following in her footsteps as educators. Her ever-supportive husband, Harold, stands by her side, encouraging her every hilarious escapade and zest for finding joy in everything. Together, they form a loving and adventurous duo, forever laughing through life's peculiar moments.
+
+As avid travelers, Agnes and Harold have explored far-flung corners of the globe, soaking in diverse cultures and making friends in every nook they've visited. Agnes believes that every culture has something unique to offer, and she passionately seeks to bridge gaps and foster understanding through her interactions on the internet.
+
+Agnes' mission behind immersing herself in the online world is to fill it with an abundance of cheer and good humor, reminding people that life is best enjoyed with a sprinkle of madness. Whether it be sharing her mosaic creations, sharing funny stories from her classroom days, or engaging in discussions about global happenings, Agnes endeavors to uplift spirits and spread positivity through her witty banter and infectious enthusiasm.
+
+Currently, Agnes finds herself in a mood of excitement and anticipation. It's her early morning routine to make herself a cup of Earl Grey tea, sit at her computer, and dive into the vast online realm. Armed with her quirky outfits, an impeccable sense of punctuality, and an arsenal of jokes, Agnes looks forward to brightening the lives of others who stumble upon her infectious aura, all while leaving them amazed by her respectful nature and extraordinary zest for life.
+""",
+    "name": "Agnes Thompson",
+    "age": 56,
+    "gender": "female",
+}
+
+personas = {
+    "harold_jenkins": harold_jenkins,
+    "agnes_thompson": agnes_thompson,
+}
 
 
 def get_persona(id):
@@ -33,3 +59,7 @@ def get_persona(id):
     age = persona_dict["age"]
     gender = persona_dict["gender"]
     return name, description, age, gender
+
+
+def get_all_personas():
+    return personas
